318e8dbc4f2a6db9ac3d847200d47757457a9554
vickymagris <victoriamagris08@gmail.com>
2026-06-01 13:26:09 -0300

    Update Usuario.java cambio por ingresar

--- Modelo/Usuario.java
+++ Modelo/Usuario.java
@@ -1,54 +1,48 @@
-package Modelo;
-public class Usuario extends Persona{
-    private String idUs;
+package modelo;
+public class Usuario extends Persona {
+    
+    //en usuario un atributo q sea carrito
+    private int idUs;
     private String emailUs;
     private String contUs;
     private Carrito carrito;
-
-     public Usuario() {
+    
+    //CONSTRUCTOR SIN PARAMETRO
+    public Usuario() {
     }
-
-    public Usuario(String idUs, String emailUs, String contUs, Carrito carrito) {
+    //CONSTRUCTOR CON PARAMETRO
+    public Usuario(int idUs, String emailUs, String contUs, Carrito carrito) {
         this.idUs = idUs;
         this.emailUs = emailUs;
         this.contUs = contUs;
         this.carrito = carrito;
     }
 
-    public Usuario(String ID_us, String email_us, String cont_us, Carrito carrito, int IDPersona, String Nombre, String Apellido, int DNI, int Telefono, String Direccion, String Localidad, String Provincia) {
-        super(IDPersona, Nombre, Apellido, DNI, Telefono, Direccion, Localidad, Provincia);
-        this.idUs = idUs;
-        this.emailUs = emailUs;
-        this.contUs = contUs;
-        this.carrito = carrito;
-    }
-
-    public String getIdUs() {
+    //GET
+    public int getIdUs() {
         return idUs;
     }
-    public void setIdUs(String idUs) {
-        this.idUs = idUs;
-    }
-
-    public String getEmailUs() {
+      public String getEmailUs() {
         return emailUs;
     }
-    public void setEmailUs(String emailUs) {
-        this.emailUs = emailUs;
-    }
-
     public String getContUs() {
         return contUs;
     }
-    public void setContUs(String contUs) {
-        this.contUs = contUs;
-    }
-
     public Carrito getCarrito() {
         return carrito;
     }
-    public void setCarrito(Carrito carrito) {
+    
+    //SET
+    public void setIdUs(int idUs) {
+        this.idUs = idUs;
+    }
+ public void setEmailUs(String emailUs) {
+        this.emailUs = emailUs;
+    }
+public void setContUs(String contUs) {
+        this.contUs = contUs;
+    }
+public void setCarrito(Carrito carrito) {
         this.carrito = carrito;
     }
-     
 }
