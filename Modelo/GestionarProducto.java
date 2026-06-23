d3dde893188820f188e0ed3040417d73dd8c5b34
Axo232526 <vigliancoalexio@gmail.com>
2026-06-23 10:13:53 -0300

    actualizamos el gestionarproducto

--- Modelo/GestionarProducto.java
+++ Modelo/GestionarProducto.java
@@ -43,41 +43,57 @@ public class GestionarProducto {
             int res = ps.executeUpdate(); 
             respuesta = res>0;
         } catch (SQLException e) {
+            System.out.println(e);
         }finally {
             try {                
                 if (ps!=null) {ps.close();}
                 if (cn!=null) {cn.close();}
             } catch (SQLException e2) {
+                 System.out.println(e2);
             }
         }    
         return respuesta;
     }
     
-    public ArrayList<Producto> BuscarColeccionproducto(){
-        ArrayList<Producto> p = new ArrayList<>();
-        try{
-            cn = c.conectar();
-            ps = cn.prepareStatement("SELECT * FROM producto");            
-            rs = ps.executeQuery();
-            while (rs.next()){
-                Producto p1 = new Producto();
-                p1.setIdproducto(rs.getInt(1));
-                p1.setNombre(rs.getString(2));
-                p1.setDescripcion(rs.getString(3));
-                p1.setFoto(rs.getString(4));
-                p.add(p1);
-            }            
-        } catch (SQLException e) {
-        }finally {
-            try {                
-                if (ps!=null) {ps.close();}
-                if (cn!=null) {cn.close();}
-                if (rs!=null) {rs.close();}
-            } catch (SQLException e2) {
-            }
-        }    
-        return p;
+   public ArrayList<Producto> BuscarProducto() {
+
+    ArrayList<Producto> p = new ArrayList<>();
+
+    try {
+
+        cn = c.conectar();
+        ps = cn.prepareStatement("SELECT * FROM producto");
+        rs = ps.executeQuery();
+
+        while (rs.next()) {
+
+            Producto p1 = new Producto();
+
+            p1.setIdproducto(rs.getInt("id_producto"));
+            p1.setStock(rs.getInt("stock"));
+            p1.setNombre(rs.getString("nombre"));
+            p1.setPrecio(rs.getInt("precio"));
+            p1.setDescripcion(rs.getString("descripcion"));
+            p1.setFoto(rs.getString("foto"));
+
+            p.add(p1);
+        }
+
+    } catch (SQLException e) {
+        System.out.println(e);
+    } finally {
+
+        try {
+            if (ps != null) ps.close();
+            if (cn != null) cn.close();
+            if (rs != null) rs.close();
+        } catch (SQLException e2) {
+        }
+
     }
+
+    return p;
+}
     public boolean modificarProducto(Producto p){
         boolean respuesta = false;
         try {              
@@ -92,12 +108,13 @@ public class GestionarProducto {
             int res = ps.executeUpdate(); 
             respuesta = res>0;
         } catch (SQLException e) {
+            System.out.println(e);
         }finally {
             try {                
                 if (ps!=null) {ps.close();}
                 if (cn!=null) {cn.close();}
             } catch (SQLException e2) {
-            }
+            System.out.println(e2);}
         }    
         return respuesta;
     }
@@ -111,13 +128,16 @@ public class GestionarProducto {
             int res = ps.executeUpdate(); 
             respuesta = res>0;
         } catch (SQLException e) {
+            
         }finally {
             try {                
                 if (ps!=null) {ps.close();}
                 if (cn!=null) {cn.close();}
             } catch (SQLException e2) {
+                
             }
         }    
         return respuesta;
     }
 }
+
